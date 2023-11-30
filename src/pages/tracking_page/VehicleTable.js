20040240dcb559e029d69004194ecdb35cde1580
Karthik002002 <karthikpanda002@gmail.com>
2023-11-30 08:39:47 +0530

    30/11 morning update

--- src/pages/tracking_page/VehicleTable.js
+++ src/pages/tracking_page/VehicleTable.js
@@ -2,18 +2,35 @@ import AdvanceTableWrapper from 'components/common/advance-table/AdvanceTableWra
 import React from 'react';
 
 const VehicleTable = () => {
-    const tableData = JSON.parse(sessionStorage.getItem('dashboardData'))
-    const columns = "empytty";
-    return (
-        <div>
-            <AdvanceTableWrapper
-            columns={columns}
-            data={tableData}
-            >
-
-            </AdvanceTableWrapper>
-        </div>
-    );
-}
+  const tableData = JSON.parse(sessionStorage.getItem('dashboardData'));
+  const columns = [
+    {
+      accessor: 'name',
+      Header: 'Vehicle Number',
+      headerProps: { className: 'pe-1' },
+      cellProps: {
+        className: 'py-2'
+      },
+      Cell: rowData => {
+        const { vehicle_reg_num } = rowData.row.original;
+        return (
+          <Link to="/e-commerce/customer-details">
+            <div className="flex-1">
+              <h5 className="mb-0 fs--1">{vehicle_reg_num}</h5>
+            </div>
+          </Link>
+        );
+      }
+    }
+  ];
+  return (
+    <div>
+      <AdvanceTableWrapper
+        columns={columns}
+        data={tableData}
+      ></AdvanceTableWrapper>
+    </div>
+  );
+};
 
 export default VehicleTable;
